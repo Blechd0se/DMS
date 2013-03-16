3473453ecb78969f6d57f0e14c8f4a225f876aa1
Blechd0se <alex.christ@hotmail.de>
2013-03-16 16:05:03 +0100

    Change sample data for compatibility

--- Tables/DML/DemoInhalte.sql
+++ Tables/DML/DemoInhalte.sql
@@ -19,6 +19,7 @@ VALUES
 
 -- NULL Recht;
 
+-- Schema "Alex"
 alter trigger "ALEX"."RECHTE_NR_TRIG" disable;
 
 INSERT INTO RECHTE
@@ -30,52 +31,107 @@ alter trigger "ALEX"."RECHTE_NR_TRIG" enable;
 
 commit;
 
-select RI_PK_NR from Rechte where ri_name = 'Lesen / Schreiben';
+-- select RI_PK_NR from Rechte where ri_name = 'Lesen / Schreiben';
 
 -- Benutzer_Gruppen
 
 INSERT INTO BENUTZER_GRUPPE
-(USGR_NAME       ,     USGR_RI_FK_RECHT1     ,     USGR_RI_FK_RECHT2)
+(USGR_NAME)
 VALUES
-('User'          ,    
-            (select RI_PK_NR from Rechte where ri_name = 'Lesen')               
-                                             ,        0             );
+('User');
 
 
 INSERT INTO BENUTZER_GRUPPE
-(USGR_NAME       ,     USGR_RI_FK_RECHT1     ,     USGR_RI_FK_RECHT2)
+(USGR_NAME)
 VALUES
-('Verwalter'     ,     
-            (select RI_PK_NR from Rechte where ri_name = 'Lesen / Schreiben')               
-                                             ,        0             );
+('Verwalter');
 
 
 INSERT INTO BENUTZER_GRUPPE
-(USGR_NAME       ,     USGR_RI_FK_RECHT1     ,     USGR_RI_FK_RECHT2)
+(USGR_NAME)
 VALUES
-('Administrator' ,     
-            (select RI_PK_NR from Rechte where ri_name = 'Löschen'),
-            (select RI_PK_NR from Rechte where ri_name = 'Lesen / Schreiben'));
+('Administrator');
 
+----- Intersect USGR_RI:
+
+-- Gruppe: Administrator:
+INSERT INTO USGR_RI
+( benutzer_gruppe_usgr_pk_nr,   rechte_RI_PK_NR )
+VALUES
+( (select USGR_PK_NR from benutzer_gruppe where USGR_NAME = 'Administrator'), 
+  (select RI_PK_NR from RECHTE where RI_Name = 'Lesen / Schreiben')
+
+);
+
+INSERT INTO USGR_RI
+( benutzer_gruppe_usgr_pk_nr,   rechte_RI_PK_NR )
+VALUES
+( (select USGR_PK_NR from benutzer_gruppe where USGR_NAME = 'Administrator'), 
+  (select RI_PK_NR from RECHTE where RI_Name = 'Löschen')
+
+);
+
+-- Gruppe: Verwalter:
+INSERT INTO USGR_RI
+( benutzer_gruppe_usgr_pk_nr,   rechte_RI_PK_NR )
+VALUES
+( (select USGR_PK_NR from benutzer_gruppe where USGR_NAME = 'Verwalter'), 
+  (select RI_PK_NR from RECHTE where RI_Name = 'Lesen / Schreiben')
+
+);
+
+-- Gruppe: User:
+INSERT INTO USGR_RI
+( benutzer_gruppe_usgr_pk_nr,   rechte_RI_PK_NR )
+VALUES
+( (select USGR_PK_NR from benutzer_gruppe where USGR_NAME = 'User'), 
+  (select RI_PK_NR from RECHTE where RI_Name = 'Lesen')
+
+);
 
 -- Benutzer anlegen:
 
 INSERT INTO BENUTZER
-(US_USERNAME      ,         US_VORNAME       ,     US_NACHNAME          ,    US_PASSWORD   ,      US_USGR_FK)
+(US_USERNAME      ,         US_VORNAME       ,     US_NACHNAME          ,    US_PASSWORD)
 VALUES
-(   'alex'        ,         'Alexander'      ,          'Christ'        ,     'Test'       , (select USGR_PK_NR from benutzer_gruppe where USGR_NAME = 'Administrator'));
+(   'alex'        ,         'Alexander'      ,          'Christ'        ,     'Test'    );
 
 
 INSERT INTO BENUTZER
-(US_USERNAME      ,         US_VORNAME       ,     US_NACHNAME          ,    US_PASSWORD   ,     US_USGR_FK)
+(US_USERNAME      ,         US_VORNAME       ,     US_NACHNAME          ,    US_PASSWORD  )
 VALUES
-(   'dummy'       ,         'Dummy'          ,          'Dummy'         ,      'Käse'      ,  (select USGR_PK_NR from benutzer_gruppe where USGR_NAME = 'User'));
+(   'dummy'       ,         'Dummy'          ,          'Dummy'         ,      'Käse'     );
 
 INSERT INTO BENUTZER
-(US_USERNAME      ,         US_VORNAME       ,     US_NACHNAME          ,    US_PASSWORD   ,     US_USGR_FK)
+(US_USERNAME      ,         US_VORNAME       ,     US_NACHNAME          ,    US_PASSWORD  )
 VALUES
-(   'Verwalter'   ,         'Peter'          ,          'Lustig'        ,      'Test'      ,  (select USGR_PK_NR from benutzer_gruppe where USGR_NAME = 'Verwalter'));
+(   'Pete'        ,         'Peter'          ,          'Lustig'        ,      'Test'     );
 
+
+-- Nutzer zu Gruppen hinzufügen:
+INSERT INTO US_USGR
+( BENUTZER_US_PK_NR,   BENUTZER_GRUPPE_USGR_PK_NR )
+VALUES
+( (select US_PK_NR from BENUTZER where US_USERNAME = 'alex'), 
+  (select USGR_PK_NR from BENUTZER_GRUPPE where USGR_NAME = 'Administrator')
+
+);
+
+INSERT INTO US_USGR
+( BENUTZER_US_PK_NR,   BENUTZER_GRUPPE_USGR_PK_NR )
+VALUES
+( (select US_PK_NR from BENUTZER where US_USERNAME = 'dummy'), 
+  (select USGR_PK_NR from BENUTZER_GRUPPE where USGR_NAME = 'User')
+
+);
+
+INSERT INTO US_USGR
+( BENUTZER_US_PK_NR,   BENUTZER_GRUPPE_USGR_PK_NR )
+VALUES
+( (select US_PK_NR from BENUTZER where US_USERNAME = 'Pete'), 
+  (select USGR_PK_NR from BENUTZER_GRUPPE where USGR_NAME = 'Verwalter')
+
+);
 
 commit;
 
